e21d2bfc438f125b382bdc9c256af18c411a061e
Jared Byrum <jaredbyrum@gmail.com>
2023-09-21 12:49:01 -0400

    all functions work properly, waiting to see if tiles update per hour

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  
   var today = dayjs();
   $('.saveBtn').click(function(){
     var description = $(this).siblings('.description').val();
@@ -16,14 +11,10 @@ $(function () {
     console.log(hour);
     localStorage.setItem(hour, description);
   })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  var currentTime = today.format('HH');
-  console.log(currentTime);
-  $('.time-block').each(function(){
+  //added interval to update everysecond
+  function updateTime(){
+    var currentTime = dayjs().format('HH');
+    $('.time-block').each(function(){
     var timeBlock = $(this).attr('id');
     if (timeBlock == currentTime){
       $(this).addClass('present');
@@ -40,10 +31,17 @@ $(function () {
     } else {
       $(this).removeClass('future');
     }
-  })  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+    }) 
+    setTimeout(updateTime, 60 * 1000)
+    }
+  updateTime();
+   
+  $('.time-block').each(function(){
+    var id = $(this).attr('id');
+    $(this).children('.description').val(localStorage.getItem(id))
+  })
+
+  var date = setInterval(function(){
+    $('#currentDay').text(dayjs().format('dddd, MMMM DD, HH:MM:ss'))
+  }, 1000)
 });
