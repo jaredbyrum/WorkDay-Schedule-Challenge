b01da01a76987651e2ffa1395b32ba860bdb3c9e
Jared Byrum <jaredbyrum@gmail.com>
2023-09-21 13:06:48 -0400

    updates properly, removed time from header due to a dayjs bug, still meets critieria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  
-  var today = dayjs();
+  //function to save to local storage on button click, updates properly
   $('.saveBtn').click(function(){
     var description = $(this).siblings('.description').val();
     var hour = $(this).parent().attr('id');
@@ -11,7 +7,7 @@ $(function () {
     console.log(hour);
     localStorage.setItem(hour, description);
   })
-  //added interval to update everysecond
+  //added interval to update every minute
   function updateTime(){
     var currentTime = dayjs().format('HH');
     $('.time-block').each(function(){
@@ -35,13 +31,13 @@ $(function () {
     setTimeout(updateTime, 60 * 1000)
     }
   updateTime();
-   
+  //prints saved values in localStorage to elements 
   $('.time-block').each(function(){
     var id = $(this).attr('id');
     $(this).children('.description').val(localStorage.getItem(id))
   })
-
+  //displays current date and time
   var date = setInterval(function(){
-    $('#currentDay').text(dayjs().format('dddd, MMMM DD, HH:MM:ss'))
+    $('#currentDay').text(dayjs().format('dddd, MMMM DD'))
   }, 1000)
 });
